c34de855bbc794ef5788aa979d3de204de3eda8e
Tess <tess.mueske@gmail.com>
2024-02-24 15:56:32 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email) {
+  const configurationObject = {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": 'application/json'
+        },
+        body: JSON.stringify({ name: name, email: email })
+      };
+    return fetch('http://localhost:3000/users', configurationObject)
+      .then(function (response) {
+         return response.json();
+        })
+      .then(function (object) {
+          document.body.textContent = object.id;
+        }) 
+      .catch(function (error) {
+        alert("ERROR");
+        document.body.textContent = error.message;
+        });
+    }
